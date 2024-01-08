ac5de69e6a5deb36354be269d650377b0fa86f3a
Speechrezz <thespeechlessmusic@gmail.com>
2024-01-08 10:29:56 -0800

    AoC 2023 day 1 starter code

---
+++ main.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+
+int main()
+{
+    // Open input text (adjust path if needed)
+    std::ifstream file("../input.txt");
+    if (!file.is_open())
+    {
+        std::cerr << "File not open!" << std::endl;
+        return 1;
+    }
+
+    // Read text
+    std::string line;
+    while (std::getline(file, line))
+    {
+        // Process each line of text here...
+        std::cout << "Line: '" << line << "'\n";
+    }
+
+    return 0;
+}
